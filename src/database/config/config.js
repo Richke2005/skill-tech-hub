028637741add3113e6ca53e761697d9cb9770001
Richke2005 <richardke18@gmail.com>
2024-10-08 19:28:48 -0300

    add online database at connect file

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
+
+const db_user = process.env.DB_USER;
+const db_password = process.env.DB_PASSWORD;
 
 //TODO: Replace the uri string with your MongoDB deployment's connection string.
 async function connect() {
     try {
-        await mongoose.connect("mongodb://localhost:27017/skilltech");
+        await mongoose.connect(`mongodb+srv://${db_user}:${db_password}@cluster-tech-hub.ckvim.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-tech-hub`);
         console.log("Connection successful to MongoDB");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
