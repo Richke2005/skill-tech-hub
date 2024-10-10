9152fcfc78084686c9465868450d0ae4730669ed
Richke2005 <richardke18@gmail.com>
2024-10-10 09:22:53 -0300

    add cors origin

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const routes = require("./routes/index.js");
 const connect = require("./database/config/config.js");
+const cors = require("cors");
 
 (async ()=> await connect())();
 
 const app = express();
 
+app.use(cors({
+    origin: "*"
+}));
 routes(app)
 
 module.exports = app;
